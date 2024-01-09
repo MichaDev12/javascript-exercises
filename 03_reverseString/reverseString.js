c933553e5a3fe356c0e858db459aa650b46539d2
Michael <122654000+MichaDev12@users.noreply.github.com>
2024-01-09 10:58:23 -0500

    03 Passing all tests :D

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let result = '';
+  let array = string.split('');
 
+  for(let i = array.length - 1; i >= 0; i--) {
+    result += array.pop();
+  }
+
+  return result;
 };
 
 // Do not edit below this line
