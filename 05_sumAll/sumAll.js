498292f6ed49ce164ff9739466e06ea254245459
Michael <122654000+MichaDev12@users.noreply.github.com>
2024-01-09 17:22:14 -0500

    05 Pass 4-6 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,12 +2,16 @@ const sumAll = function (num1, num2) {
   let min;
   let max;
 
-  if (num1 > num2) {
-    max = num1;
-    min = num2;
+  if (num1 > 0 && num2 > 0 && typeof num1 == "number" && typeof num2 == "number") {
+    if (num1 > num2) {
+      max = num1;
+      min = num2;
+    } else {
+      max = num2;
+      min = num1;
+    }
   } else {
-    max = num2;
-    min = num1;
+    return 'ERROR';
   }
 
   let finalSum = 0;
