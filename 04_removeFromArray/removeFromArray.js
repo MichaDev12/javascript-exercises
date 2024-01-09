0cff75b535f23dff4f5162c8ba70ef415e82794d
Michael <122654000+MichaDev12@users.noreply.github.com>
2024-01-09 12:33:41 -0500

    04 Pass the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arg) {
+  let newArray = [];
+  
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] != arg) {
+      newArray.push(array[i]);
+    }
+  }
 
+  return newArray;
 };
 
 // Do not edit below this line
